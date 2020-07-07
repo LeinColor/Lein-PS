967c0e8e0df1fee6cfecc6ed256ba3e8a6cbd589
Lein <leincolor@gmail.com>
2020-07-07 20:31:08 +0900

    14612 14620

---
+++ 14612. 김식당.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+const int MAX_SIZE = 101;
+
+struct Table {
+	int number;
+	int time;
+};
+
+bool cmp(const Table& a, const Table& b)
+{
+	if (a.time == b.time)
+		return a.number < b.number;
+	return a.time < b.time;
+}
+
+void shift(Table(&table)[MAX_SIZE], int start, int end)
+{
+	int index = start;
+
+	while (index < end - 1) {
+		table[index].number = table[index + 1].number;
+		table[index].time = table[index + 1].time;
+		index++;
+	}
+}
+
+void print(Table (&table)[MAX_SIZE], int end)
+{
+	if (end == 0) {
+		cout << "sleep\n";
+		return;
+	}
+
+	for (int i = 0; i < end; i++) {
+		cout << table[i].number << " ";
+	}
+	cout << '\n';
+}
+
+int find(Table (&table)[MAX_SIZE], int target)
+{
+	for (int i = 0; i < MAX_SIZE; i++) {
+		if (table[i].number == target)
+			return i;
+	}
+	return -1;
+}
+
+Table tables[MAX_SIZE];
+int main()
+{
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	int N, M;
+	cin >> N >> M;
+
+	int count = 0;
+
+	while (N--) {
+		string query;
+		cin >> query;
+
+		if (query == "order") {
+			int order_num, order_time;
+			cin >> order_num >> order_time;
+			tables[count].number = order_num;
+			tables[count].time = order_time;
+			++count;
+		}
+		else if (query == "sort") {
+			sort(tables, tables + count, cmp);
+		}
+		else if (query == "complete") {
+			int complete_num;
+			cin >> complete_num;
+
+			int target_index = find(tables, complete_num);
+			shift(tables, target_index, count);
+			--count;
+		}
+
+		print(tables, count);
+	}
+
+	return 0;
+}
---
+++ 14620. 꽃길.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+const int MAX_LEN = 10 + 1;
+
+int N;
+int cost[MAX_LEN][MAX_LEN];
+bool visited[MAX_LEN][MAX_LEN];
+int dx[] = { 0,0,0,-1,1 };
+int dy[] = { 0,-1,1,0,0 };
+int answer = 1234567890;
+int update_visited(int x, int y, bool flag)
+{
+	int total_cost = 0;
+
+	for (int i = 0; i < 5; i++) {
+		int next_dx = x + dx[i];
+		int next_dy = y + dy[i];
+
+		visited[next_dx][next_dy] = flag;
+		
+		total_cost += cost[next_dx][next_dy];
+	}
+
+	return total_cost;
+}
+void dfs(int depth, int x, int y, int total_cost)
+{
+	if (depth == 3) {
+		answer = min(answer, total_cost);
+		return;
+	}
+
+	// check if it is overlapped
+	bool is_overlapped = false;
+
+	for (int i = 0; i < 5; i++) {
+		int next_dx = x + dx[i];
+		int next_dy = y + dy[i];
+
+		if (next_dx <= 0 || next_dx > N || next_dy <= 0 || next_dy > N || visited[next_dx][next_dy]) {
+			is_overlapped = true;
+			break;
+		}
+	}
+
+	// if it is not overlapped, call next dfs
+	if (!is_overlapped) {
+		int additional_cost = update_visited(x, y, true);
+
+		for (int i = 2; i <= N - 1; i++) {
+			for (int j = 2; j <= N - 1; j++) {
+				dfs(depth + 1, i, j, total_cost + additional_cost);
+			}
+		}
+
+		// back to prev visited state
+		update_visited(x, y, false);
+	}
+}
+
+int main()
+{
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> cost[i][j];
+		}
+	}
+	
+	for (int i = 2; i <= N - 1; i++) {
+		for (int j = 2; j <= N - 1; j++) {
+			dfs(0, i, j, 0);
+		}
+	}
+
+	cout << answer;
+	return 0;
+}
