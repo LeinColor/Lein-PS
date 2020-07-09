73149a7fe96edf4ba53163fa4026d5e8b4fd9306
Lein <leincolor@gmail.com>
2020-07-09 23:22:47 +0900

    CR654 Div2 Magical Sticks

--- [CR654 Div.2] A.Magical Sticks.cpp
+++ [CR654 Div.2] A.Magical Sticks.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int len;
+		cin >> len;
+		cout << (len + 1) / 2 << '\n';
+	}
 	return 0;
 }
