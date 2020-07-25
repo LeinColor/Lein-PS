8b4ed693f5faa5337c999556b469bf45bfaa2cee
Lein <leincolor@gmail.com>
2020-07-25 22:06:05 +0900

    BOJ

---
+++ [BOJ] 2864. 5와 6의 차이.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int max_answer, min_answer;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	for (int i = 0; i < 2; i++) {
+		int n;
+		cin >> n;
+		for (int j = 1; n > 0; j *= 10, n /= 10) {
+			if (n % 10 == 5 || n % 10 == 6) {
+				max_answer += 6 * j;
+				min_answer += 5 * j;
+			}
+			else {
+				max_answer += (n % 10) * j;
+				min_answer += (n % 10) * j;
+			}
+		}
+	}
+	cout << min_answer << " " << max_answer;
+
+	return 0;
+}
